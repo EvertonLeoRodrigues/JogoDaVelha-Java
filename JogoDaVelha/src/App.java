042616a62f17429878885e552e3115885063ceb1
Leonel <leonel.rodrigues302@outlook.com>
2024-03-31 15:26:38 -0300

    "Quase pronto"

--- JogoDaVelha/src/App.java
+++ JogoDaVelha/src/App.java
@@ -7,32 +7,38 @@ class App {
     
     public static void main(String[] args) throws Exception {
         Player winner;
+        Player playerOne;
+        Player playerTwo;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Type the name of the first player: ");
+        String playerOneName = sc.nextLine();
+        System.out.print("Now, type the name of the second player: ");
+        String playerTwoName = sc.nextLine();
+        System.out.println();
+        playerOne = new Player(playerOneName, 'X');
+        playerTwo = new Player(playerTwoName, 'O');
 
         while (true) {
-            Scanner sc = new Scanner(System.in);
-
-            System.out.print("Type the name of the first player: ");
-            String playerOneName = sc.nextLine();
-            System.out.print("New, type the name of the second player: ");
-            String playerTwoName = sc.nextLine();
-            System.out.println("");
-
-            Player playerOne = new Player(playerOneName, 'X');
-            Player playerTwo = new Player(playerTwoName, 'O');
             
-            System.out.println("Enter the board size: ");
-            int dimension = sc.nextInt();
+            
+            int dimension;
+
+            do {
+                System.out.println("Enter the board size: ");
+                dimension = sc.nextInt();    
+            } while (dimension<3);
+            
 
             NoughtsAndCrosses board = new NoughtsAndCrosses(dimension);
 
             boolean player1Turn = true;
 
             while (true) {
-                if(player1Turn){
-                    System.out.println("Your turn "+playerOne.getName());
-                }else{
-                    System.out.println("Your turn "+playerTwo.getName());
-                }
+                if(player1Turn) System.out.println("Your turn "+playerOne.getName());
+                else System.out.println("Your turn "+playerTwo.getName());
+                
 
                 System.out.println();
                 int row, col;
@@ -51,12 +57,13 @@ class App {
                 System.out.println(board);
                 System.out.println();
 
+
                 if(board.checkWinner()){
                     if(player1Turn){
-                        System.out.println("Player one has triumphed!");
+                        System.out.println(String.format("Player one %s has triumphed!", playerOne.getName()));
                         playerOne.setScore(playerOne.getScore()+1);
                     }else{
-                        System.out.println("Player two has triumphed!");
+                        System.out.println(String.format("Player two %s has triumphed!", playerTwo.getName()));
                         playerTwo.setScore(playerTwo.getScore()+1);
                     }
                     
@@ -69,23 +76,31 @@ class App {
             System.out.println("Final score! ");
             System.out.println(playerOne);
             System.out.println();
-            System.out.println(playerTwoName+"\n");
+            System.out.println(playerTwo+"\n");
 
             System.out.println("Do you want go again? (s/n)");
             String answer = sc.next();
-            if (!answer.equalsIgnoreCase("s")) {
-                if (playerOne.getScore()>playerTwo.getScore()) {
-                    winner = playerOne;
-                }else{
-                    winner = playerTwo;
-                }
+            sc.close();
 
-                File f = new File("Result.txt");
-                FileWriter fr = new FileWriter(f);
-                PrintWriter out = new PrintWriter(fr);
-                out.println("Winner: "+winner.toString());
+            if (!answer.equalsIgnoreCase("s")) {
                 break;
             }
         }
+
+        try {
+            if (playerOne.getScore()>playerTwo.getScore()) winner = playerOne;
+                else winner = playerTwo;
+                
+
+            File f = new File("Result.txt");
+            FileWriter fr = new FileWriter(f);
+            PrintWriter out = new PrintWriter(fr);
+            out.println("Winner: " + winner);
+            out.close();
+            fr.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
     }
 }
--- JogoDaVelha/src/classes/NoughtsAndCrosses.java
+++ JogoDaVelha/src/classes/NoughtsAndCrosses.java
@@ -13,59 +13,64 @@ public class NoughtsAndCrosses{
     }
 
     public boolean makeAMove(int row, int col, char symbol) throws IllegalArgumentException, PositionFilledException{
-        if (symbol != 'X'  && symbol != 'O') {
-            throw new IllegalArgumentException();
-        }
 
-        if(board[row][col] != '\0'){
-            throw new PositionFilledException("This position is already filled! ");
-        }
+        if (symbol != 'X'  && symbol != 'O') throw new IllegalArgumentException();    
+
+        if(board[row][col] != '\0') throw new PositionFilledException("This position is already filled! ");
 
         board[row][col] = symbol;
+
         return true;
     }
 
-    public boolean checkWinner(){
 
-        char primaryDiagonal = board[0][0];
-        char secundaryDiagonal = board[0][dimension-1];
-
-        for (int i = 1; i < dimension; i++) {
-            if(board[i][i]!=primaryDiagonal || board[i][dimension-1-i]!=secundaryDiagonal){
-                break;
-            }
-
-            if (i == dimension-1) {
-                return true;
-            }
-        } 
-
+    private boolean checkRowCol(){
         for (int i = 0; i < dimension; i++) {
-            char currentRow = board[i][0];
-            char currentCol = board[0][i];
+            char startRow = board[i][0];
+            char startCol = board[0][i];
+            boolean wonRow = true;
+            boolean wonCol = true;
 
             for (int j = 1; j < dimension; j++) {
-                if (board[i][j]!=currentRow || board[j][i]!=currentCol) {
-                    break;
-                }
-
-                if (j == dimension-1) {
-                    return true;
-                }
+                if (board[i][j] != startRow || startRow == '\0') wonRow = false;
+                if (board[j][i] != startCol || startCol == '\0') wonCol = false;              
             }
-        }
 
+            if (wonRow || wonCol) return true;
+        }
         return false;
     }
 
+    private boolean checkDiagonal(){
+        char primaryDiagonal = board[0][0];
+        char secundaryDiagonal = board[0][dimension - 1];
+        boolean wonPrimaryDiagonal = true;
+        boolean wonSecundaryDiagonal = true;
+
+        for (int i = 1; i < dimension; i++) {
+                if (board[i][i] != primaryDiagonal || primaryDiagonal == '\0') wonPrimaryDiagonal = false;
+                if (board[i][dimension - 1 - i] != secundaryDiagonal || secundaryDiagonal == '\0') wonSecundaryDiagonal = false;    
+        }
+
+        if (wonPrimaryDiagonal || wonSecundaryDiagonal) return true;
+        else return false;
+    }
+    
+
+    public boolean checkWinner(){
+        if (checkRowCol() || checkDiagonal()) return true;
+        else return false;
+    }
+
+
+
     @Override
     public String toString() {
+
         StringBuilder sb = new StringBuilder();
+
         for (char[] line : board) {
-
-
             for (int i = 0; i < dimension; i++) {
-                
                 sb.append(" ").append("-");
             } 
 
@@ -74,16 +79,11 @@ public class NoughtsAndCrosses{
             for (int i = 0; i < line.length; i++) {
                 sb.append("|").append(line[i]=='\0'?" ": line[i]);
             } 
-            
-            
-            sb.append("|\n");
-            
-            
 
+            sb.append("|\n");
         }
 
         for (int i = 0; i < dimension; i++) {
-           
             sb.append(" ").append("-");
         }
 
